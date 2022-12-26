97ba4d423191b9c03a0654b38bce0f35a79d751f
Sofya Kyzylova <121377923+SofyaKyzylova@users.noreply.github.com>
2022-12-26 13:43:19 +0500

    Add files via upload

---
+++ Project1/src/main/kotlin/Main.kt
@@ -0,0 +1,44 @@
+fun main() {
+    println("Здравствуйте!")
+    val name = "Кызылова Софья Андреевна"
+    val spec = "Информатика и вычислительная техника"
+    println("Меня зовут $name, я учусь в Тюменском индустриальном университете по специальности $spec \n")
+
+
+    val a : Int = 100
+    println("Переменная $a имеет тип ${a::class.java.typeName}")
+
+    val pi : Double = 3.14
+    println("Переменная $pi имеет тип ${pi::class.java.typeName}")
+
+    val flag : Boolean = true
+    println("Переменная $flag имеет  ${flag::class.java.typeName}")
+
+    var myChar : Char = 'a'
+    println("Переменная $myChar имеет тип ${myChar::class.java.typeName}")
+
+    val str : String = "abcdefg"
+    println("Переменная $str имеет тип ${str::class.java.typeName}")
+
+    val bt : Byte = 64
+    println("Переменная $bt имеет тип ${bt::class.java.typeName}")
+
+    var arr = IntArray(5) { it + 1 }
+    println("Массив переменных типа Int:")
+    arr.forEach { println(it) }
+    println("\n")
+
+
+    val myString = readlnOrNull()
+    println("Введенная строка: $myString")
+    //println("Длина строки =  ${myString.length}")
+    //println("Третий символ строки = ${myString.charAt(3)}")
+    //myString.equals(str)
+    //myString.equalsIgnoreCase(str)
+    //val startsWith : Boolean = myString.startsWith("A")
+    //val endsWith : Boolean = myString.endsWith("!")
+    //val containsP = myString.contains("hi")
+    //val newString : String = myString.concat(" . The end");
+    //val newString : String = myString.replace('i', 'a');
+
+}
