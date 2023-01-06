366cb934bb50ae84eb42feed3ce806312b3dcadc
Sofya Kyzylova <121377923+SofyaKyzylova@users.noreply.github.com>
2023-01-06 17:12:57 +0500

    Update Main.kt

--- Project1/src/main/kotlin/Main.kt
+++ Project1/src/main/kotlin/Main.kt
@@ -1,19 +1,19 @@
 fun main() {
     //ЗАДАНИЕ 1
     println("Здравствуйте!")
-    val name = "Кызылова Софья Андреевна"
-    val spec = "Информатика и вычислительная техника"
-    println("Меня зовут $name, я учусь в Тюменском индустриальном университете на специальности $spec \n")
+    var name = "Кызылова Софья Андреевна"
+    var spec = "Информатика и вычислительная техника"
+    println("Меня зовут $name, я учусь на 3 курсе в Тюменском индустриальном университете на специальности $spec \n")
 
     
     //ЗАДАНИЕ 2
-    val a : Int = 100
+    var a : Int = 100
     println("Переменная $a имеет тип ${a::class.java.typeName}")
 
-    val pi : Double = 3.14
+    var pi : Double = 3.14
     println("Переменная $pi имеет тип ${pi::class.java.typeName}")
 
-    val flag : Boolean = true
+    var flag : Boolean = true
     println("Переменная $flag имеет  ${flag::class.java.typeName}")
 
     var myChar : Char = 'a'
@@ -22,26 +22,52 @@ fun main() {
     val str : String = "abcdefg"
     println("Переменная $str имеет тип ${str::class.java.typeName}")
 
-    val bt : Byte = 64
+    var bt : Byte = 64
     println("Переменная $bt имеет тип ${bt::class.java.typeName}")
 
     var arr = IntArray(5) { it + 1 }
     println("Массив переменных типа Int:")
     arr.forEach { println(it) }
     println("\n")
-    
+
     
     //ЗАДАНИЕ 3
+    println("Введите строку: ")
     val myString = readlnOrNull()
-    println("Введенная строка: $myString")
-    //val n : Int = myString.length()
-    //println("Длина строки =  ${myString.length}")
-    //println("Третий символ строки = ${myString.charAt(3)}")
-    //myString.equals(str)
-    //myString.equalsIgnoreCase(str)
-    //val startsWith : Boolean = myString.startsWith("A")
-    //val endsWith : Boolean = myString.endsWith("!")
-    //val containsP = myString.contains("hi")
-    //val newString : String = myString.concat(" . The end");
-    //val newString : String = myString.replace('i', 'a');
+    println("Введенная строка: $myString \n")
+
+    if(myString!!.isEmpty())
+        println("Строка пустая! \n")
+    else
+    {
+        println("Строка не пустая! \n")
+
+        println("${myString!!.uppercase()}")
+        println("${myString!!.lowercase()} \n")
+
+        println("Длина строки =  ${myString!!.length} \n")
+        //println("Третий символ строки = ${myString!!.get(2)} \n")
+
+        var startsWith : Boolean = myString.startsWith("A")
+        if(startsWith) println("Введенная строка начинается с символа А")
+        else println("Введенная строка НЕ начинается с символа А")
+
+        var endsWith : Boolean = myString.endsWith("!")
+        if(endsWith) println("Введенная строка заканчивается символом ! \n")
+        else println("Введенная строка НЕ заканчивается символом ! \n")
+
+        var containsHi = myString.contains("hi")
+        if(containsHi)
+            println("myString содержит подстроку hi \n")
+        else println("myString НЕ содержит подстроку hi \n")
+
+        val addString = "... The end";
+        val sb = StringBuilder()
+        sb.append(myString).append(addString)
+        val newString = sb.toString()
+        println("Новая строка: $newString \n")
+
+        val nString : String = myString!!.replace('i', 'a');
+        print(nString)
+    }
 }
